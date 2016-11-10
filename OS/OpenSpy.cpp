cc937be0ed1e3829bdc61a7ea5b0d4edd97eae10
Andrew Learn <chcniz@gmail.com>
2016-11-09 19:20:21 -0500

    added internal redis context for gamename lookup

--- OS/OpenSpy.cpp
+++ OS/OpenSpy.cpp
@@ -4,6 +4,7 @@
 
 namespace OS {
 	redisContext *redis_connection = NULL;
+	redisContext *redis_internal_connection = NULL;
 	void Init() {
 		
 		struct timeval t;
@@ -11,39 +12,41 @@ namespace OS {
 		t.tv_sec = 3;
 
 		redis_connection = redisConnectWithTimeout("127.0.0.1", 6379, t);
+
+		redis_internal_connection = redisConnectWithTimeout("127.0.0.1", 6379, t);
 	}
 	OS::GameData GetGameByRedisKey(const char *key) {
 		GameData game;
 		redisReply *reply;	
-		freeReplyObject(redisCommand(OS::redis_connection, "SELECT %d", ERedisDB_Game));
+		freeReplyObject(redisCommand(OS::redis_internal_connection, "SELECT %d", ERedisDB_Game));
 
-		reply = (redisReply *)redisCommand(OS::redis_connection, "HGET %s gameid", key);
+		reply = (redisReply *)redisCommand(OS::redis_internal_connection, "HGET %s gameid", key);
 		if (reply->type == REDIS_REPLY_STRING)
 			game.gameid = atoi(reply->str);
 		freeReplyObject(reply);
 
 
-		reply = (redisReply *)redisCommand(OS::redis_connection, "HGET %s secretkey", key);
+		reply = (redisReply *)redisCommand(OS::redis_internal_connection, "HGET %s secretkey", key);
 		if (reply->type == REDIS_REPLY_STRING)
 			strcpy(game.secretkey, reply->str);
 		freeReplyObject(reply);
 
-		reply = (redisReply *)redisCommand(OS::redis_connection, "HGET %s description", key);
+		reply = (redisReply *)redisCommand(OS::redis_internal_connection, "HGET %s description", key);
 		if (reply->type == REDIS_REPLY_STRING)
 			strcpy(game.description, reply->str);
 		freeReplyObject(reply);
 
-		reply = (redisReply *)redisCommand(OS::redis_connection, "HGET %s gamename", key);
+		reply = (redisReply *)redisCommand(OS::redis_internal_connection, "HGET %s gamename", key);
 		if (reply->type == REDIS_REPLY_STRING)
 			strcpy(game.gamename, reply->str);
 		freeReplyObject(reply);
 
-		reply = (redisReply *)redisCommand(OS::redis_connection, "HGET %s disabled_services", key);
+		reply = (redisReply *)redisCommand(OS::redis_internal_connection, "HGET %s disabled_services", key);
 		if (reply->type == REDIS_REPLY_STRING)
 			game.disabled_services = atoi(reply->str);
 		freeReplyObject(reply);
 
-		reply = (redisReply *)redisCommand(OS::redis_connection, "HGET %s queryport", key);
+		reply = (redisReply *)redisCommand(OS::redis_internal_connection, "HGET %s queryport", key);
 		if (reply->type == REDIS_REPLY_STRING)
 			game.queryport = atoi(reply->str);
 		freeReplyObject(reply);
@@ -53,10 +56,10 @@ namespace OS {
 	}
 	OS::GameData GetGameByName(const char *from_gamename) {
 		redisReply *reply;
-		freeReplyObject(redisCommand(OS::redis_connection, "SELECT %d", ERedisDB_Game));
+		freeReplyObject(redisCommand(OS::redis_internal_connection, "SELECT %d", ERedisDB_Game));
 		OS::GameData ret;
 		memset(&ret, 0, sizeof(ret));
-		reply = (redisReply *)redisCommand(OS::redis_connection, "KEYS %s:*",from_gamename);
+		reply = (redisReply *)redisCommand(OS::redis_internal_connection, "KEYS %s:*",from_gamename);
 		if (reply->type == REDIS_REPLY_ARRAY) {
 			for (int j = 0; j < reply->elements; j++) {
 				ret = GetGameByRedisKey(reply->element[j]->str);
@@ -68,10 +71,10 @@ namespace OS {
 	}
 	OS::GameData GetGameByID(int gameid) {
 		redisReply *reply;
-		freeReplyObject(redisCommand(OS::redis_connection, "SELECT %d", ERedisDB_Game));
+		freeReplyObject(redisCommand(OS::redis_internal_connection, "SELECT %d", ERedisDB_Game));
 		OS::GameData ret;
 		memset(&ret, 0, sizeof(ret));
-		reply = (redisReply *)redisCommand(OS::redis_connection, "KEYS *:%d:", gameid);
+		reply = (redisReply *)redisCommand(OS::redis_internal_connection, "KEYS *:%d:", gameid);
 		if (reply->type == REDIS_REPLY_ARRAY) {
 			for (int j = 0; j < reply->elements; j++) {
 				ret = GetGameByRedisKey(reply->element[j]->str);
