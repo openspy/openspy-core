912a9b1e06be24c5ab043ba2b9bcc0b6ea2b7d9b
Andrew Learn <chc@thmods.com>
2016-11-18 23:30:26 -0500

    moved v2 specific macros to v2peer.h

--- serverbrowsing/server/SBPeer.h
+++ serverbrowsing/server/SBPeer.h
@@ -3,32 +3,6 @@
 #include "../main.h"
 #include "MMQuery.h"
 
-//message types for outgoing requests
-#define SERVER_LIST_REQUEST		0
-#define SERVER_INFO_REQUEST		1
-#define SEND_MESSAGE_REQUEST	2
-#define KEEPALIVE_REPLY			3
-#define MAPLOOP_REQUEST			4
-#define PLAYERSEARCH_REQUEST	5
-
-//message types for incoming requests
-#define PUSH_KEYS_MESSAGE		1
-#define PUSH_SERVER_MESSAGE		2
-#define KEEPALIVE_MESSAGE		3
-#define DELETE_SERVER_MESSAGE	4
-#define MAPLOOP_MESSAGE			5
-#define PLAYERSEARCH_MESSAGE	6
-
-//server list update options
-#define SEND_FIELDS_FOR_ALL		1
-#define NO_SERVER_LIST			2
-#define PUSH_UPDATES			4
-#define SEND_GROUPS				32
-#define NO_LIST_CACHE			64
-#define LIMIT_RESULT_COUNT		128
-
-#define ALTERNATE_SOURCE_IP 8
-
 //Maximum length for the SQL filter string
 #define MAX_FILTER_LEN 511
 
@@ -36,30 +10,12 @@
 
 #define MAX_OUTGOING_REQUEST_SIZE (MAX_FIELD_LIST_LEN + MAX_FILTER_LEN + 255)
 
-#define LIST_CHALLENGE_LEN 8
-
-
-//game server flags
-#define UNSOLICITED_UDP_FLAG	1
-#define PRIVATE_IP_FLAG			2
-#define CONNECT_NEGOTIATE_FLAG	4
-#define ICMP_IP_FLAG			8
-#define NONSTANDARD_PORT_FLAG	16
-#define NONSTANDARD_PRIVATE_PORT_FLAG	32
-#define HAS_KEYS_FLAG					64
-#define HAS_FULL_RULES_FLAG				128
-
-
-
-
 
 namespace SB {
 	struct sServerListReq {
 		uint8_t protocol_version;
 		uint8_t encoding_version;
 		uint32_t game_version;
-		
-		char challenge[LIST_CHALLENGE_LEN];
 		
 		const char *filter;
 		//const char *field_list;
--- serverbrowsing/server/V2Peer.h
+++ serverbrowsing/server/V2Peer.h
@@ -2,6 +2,47 @@
 #define _V2PEER_H
 #include "SBPeer.h"
 #include "SBDriver.h"
+//message types for outgoing requests
+#define SERVER_LIST_REQUEST		0
+#define SERVER_INFO_REQUEST		1
+#define SEND_MESSAGE_REQUEST	2
+#define KEEPALIVE_REPLY			3
+#define MAPLOOP_REQUEST			4
+#define PLAYERSEARCH_REQUEST	5
+
+//message types for incoming requests
+#define PUSH_KEYS_MESSAGE		1
+#define PUSH_SERVER_MESSAGE		2
+#define KEEPALIVE_MESSAGE		3
+#define DELETE_SERVER_MESSAGE	4
+#define MAPLOOP_MESSAGE			5
+#define PLAYERSEARCH_MESSAGE	6
+
+//server list update options
+#define SEND_FIELDS_FOR_ALL		1
+#define NO_SERVER_LIST			2
+#define PUSH_UPDATES			4
+#define SEND_GROUPS				32
+#define NO_LIST_CACHE			64
+#define LIMIT_RESULT_COUNT		128
+
+#define ALTERNATE_SOURCE_IP 8
+
+
+
+#define LIST_CHALLENGE_LEN 8
+
+
+//game server flags
+#define UNSOLICITED_UDP_FLAG	1
+#define PRIVATE_IP_FLAG			2
+#define CONNECT_NEGOTIATE_FLAG	4
+#define ICMP_IP_FLAG			8
+#define NONSTANDARD_PORT_FLAG	16
+#define NONSTANDARD_PRIVATE_PORT_FLAG	32
+#define HAS_KEYS_FLAG					64
+#define HAS_FULL_RULES_FLAG				128
+
 namespace SB {
 		class V2Peer : public Peer {
 			public:
