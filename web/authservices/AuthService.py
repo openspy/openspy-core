a46036e573208d19f4c43b82f382071eb29714fb
Andrew Learn <chc@thmods.com>
2016-12-02 22:04:34 -0500

    added backend jwt auth service

--- web/authservices/AuthService.py
+++ web/authservices/AuthService.py
@@ -6,7 +6,13 @@ from M2Crypto import RSA, BIO
 import md5, struct, os
 
 from collections import OrderedDict
+import jwt
 
+import httplib, urllib
+
+LOGIN_SERVER = 'localhost:80'
+LOGIN_SCRIPT = '/backend/auth'
+SECRET_AUTH_KEY = "dGhpc2lzdGhla2V5dGhpc2lzdGhla2V5dGhpc2lzdGhla2V5"
 
 LOGIN_RESPONSE_SUCCESS = 0
 LOGIN_RESPONSE_SERVERINITFAILED = 1
@@ -16,22 +22,6 @@ LOGIN_RESPONSE_INVALID_PROFILE = 4
 LOGIN_RESPONSE_UNIQUE_NICK_EXPIRED = 5
 LOGIN_RESPONSE_DB_ERROR = 6
 LOGIN_RESPONSE_SERVER_ERROR = 7
-
-private_key =  "-----BEGIN RSA PRIVATE KEY-----\n" +\
-"MIICXQIBAAKBgQDEDRjQu5OOTXpg1IJfHYVhnJxGRLQWLvFCIIVjULZ2MU07uAta\n" +\
-"wuOwQAEUt/ZCm0T14brZ8rsJfR3V3OJxrEUEb6TsIPDIl85fL3xctlITVe9ORpPq\n" +\
-"Ickg9Q2GXPHvNaeBMyzygQ8sCyTJ62SxT1FmNA3xs08osENXPGIEUaJ8OwIDAQAB\n" +\
-"AoGABXisGZ8yhgUphixIGylywH+jaN6f/AKBXywTLOtivDeyBRmkz3qi6hdPMGnV\n" +\
-"6JP2v7n2AgEhMSmZvI82jp+VKXcDsbcupZMp4NqFSC03Li2cye5aoFHBWKMkLHfF\n" +\
-"TnfvvaO5+vra2M56l2HpPfoMEyOk9OJ/FlepxIpXxNCZQVkCQQDqdy9H8AnXVs+G\n" +\
-"/EulUefCx31Ksla3Lf8+JLJOerfJBES3n8/0mRqvtNvSNHcQWUuqyLzB5hYQaEnq\n" +\
-"LV1HYavFAkEA1g6z6DVoI4AYxSywR4mz7o8JOxF9hzTCv50pKhIgb4wzdZX+dc+N\n" +\
-"4ePXoiD8nrMS4Jixc03NPFLV4O3OmW4H/wJBALXQXjWmibsWci72jaJQ9SsxjpLR\n" +\
-"4DSD0p3ZzvrUZpfWW4MYxiWiY/NEiAFk9b8Tv31b1CN3zDxE4qxZKTAlKRECQHAc\n" +\
-"0UN8vWdijxauekFtsQzwY6BJX9qx2pJraQT863ohD0612cmwhJpcMDNdXZJtLiTu\n" +\
-"NHq0tBq1NAoT45Jem9cCQQCgu2P7MCwKJP9ZUMLKFq7G+Zlqeb9ENtDQICjEfPvP\n" +\
-"HyvTGVsGf0jSgo5DK78oGVDbcaazvCQAe2EzXfh2KzgL\n" +\
-"-----END RSA PRIVATE KEY-----"
 
 auth_user_dir = OrderedDict()
 
@@ -66,8 +56,47 @@ def get_user_by_id(id):
     ret['profilenick'] = 'Bobbeh'
     ret['uniquenick'] = 'Bobby'
     ret['cdkeyhash'] = '9f86d081884c7d659a2feaa0c55ad015'.upper()
-
     return ret
+
+
+#will be used if LOGIN_RESPONE_* differs from backend auth
+def convert_reason_code(reason):
+    return str(reason)
+
+def try_authenticate(login_data):
+    login_data["user_login"] = False
+    login_data["profile_login"] = False
+    params = jwt.encode(login_data, SECRET_AUTH_KEY, algorithm='HS256')
+    #params = urllib.urlencode(params)
+    
+    headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
+    conn = httplib.HTTPConnection(LOGIN_SERVER)
+    conn.request("POST", LOGIN_SCRIPT, params, headers)
+    response = conn.getresponse().read()
+
+    response = jwt.decode(response, SECRET_AUTH_KEY, algorithm='HS256')
+
+    print("Got resp: {}".format(response))
+
+    if "success" in response and response["success"] == True:
+        profile = response['profile']
+        response['profile'] = OrderedDict()
+        #stupid copy for XML reader of web services C SDK(bug where reading out of order = invalid cert)
+        response['profile']['partnercode'] = profile['partnercode']
+        response['profile']['namespaceid'] = profile['namespaceid']
+        response['profile']['userid'] = profile['userid']
+        response['profile']['profileid'] = profile['profileid']
+        response['profile']['expiretime'] = profile['expiretime']
+        response['profile']['profilenick'] = profile['profilenick']
+        response['profile']['uniquenick'] = profile['uniquenick']
+        response['profile']['cdkeyhash'] = '9f86d081884c7d659a2feaa0c55ad015'.upper()
+        return response
+
+    #failed, delete profile if exists and send error
+    if 'profile' in response:
+        del response['profile']
+
+    return response
 
 
 def handle_remoteauth_login(xml_tree, rsa):
@@ -85,7 +114,7 @@ def handle_profile_login(xml_tree, rsa):
     login_result = ET.SubElement(body, 'ns1:LoginProfileResult')
 
     response_code_node = ET.SubElement(login_result, 'ns1:responseCode')
-    response_code_node.text = str(LOGIN_RESPONSE_SUCCESS)
+    
 
 
     #auth stuff
@@ -101,33 +130,7 @@ def handle_profile_login(xml_tree, rsa):
     version_node = ET.SubElement(certificate_node, 'ns1:version')
     version_node.text = '1'
 
-    auth_user_dir = get_user_by_id(111)
-    #user info
-    for k,v in auth_user_dir.items():
-        node = ET.SubElement(certificate_node, 'ns1:{}'.format(k))
-        node.text = v
-
-    #encrypted server data
-    peerkeymodulus_node = ET.SubElement(certificate_node, 'ns1:peerkeymodulus')
-    rsa_modulus = binascii.hexlify(rsa.n)
-    peerkeymodulus_node.text = rsa_modulus[-128:].upper()
-
-    peerkeyexponent_node = ET.SubElement(certificate_node, 'ns1:peerkeyexponent')
-    rsa_exponent = binascii.hexlify(rsa.e)
-    peerkeyexponent_node.text = rsa_exponent[-6:]
-
-    serverdata_node = ET.SubElement(certificate_node, 'ns1:serverdata')
-
-    server_data = os.urandom(128)
-    serverdata_node.text = binascii.hexlify(server_data)
-
-    signature_node = ET.SubElement(certificate_node, 'ns1:signature')
-
-    peerkey = {}
-    peerkey['exponent'] = peerkeyexponent_node.text
-    peerkey['modulus'] = peerkeymodulus_node.text
-    signature_node.text = generate_signature(rsa, int(length_node.text), int(version_node.text), auth_user_dir, peerkey, server_data, True)
-
+    #get login info
     profilenick_node = xml_tree.find('{http://gamespy.net/AuthService/}profilenick')
     profilenick = profilenick_node.text
 
@@ -140,40 +143,21 @@ def handle_profile_login(xml_tree, rsa):
     namespaceid_node = xml_tree.find('{http://gamespy.net/AuthService/}partnercode')
     namespaceid = namespaceid_node.text
 
+    #decrypt pw
     encrypted_pass = xml_tree.find('{http://gamespy.net/AuthService/}password').find('{http://gamespy.net/AuthService/}Value')
     password = rsa.private_decrypt(binascii.unhexlify(encrypted_pass.text), RSA.pkcs1_padding) 
-    print("Decrypted pass: {}".format(password))
 
-    return resp_xml
+    #try auth
+    auth_user_dir = try_authenticate({'email': email, 'profilenick': profilenick, 'password': password, 'namespaceid': int(namespaceid), 'partnercode': int(partnercode), 'hash_type': 'plain'})
+    if 'profile' in auth_user_dir:
+        response_code_node.text = str(LOGIN_RESPONSE_SUCCESS)
+        #populate user info
+        for k,v in auth_user_dir['profile'].items():
+            node = ET.SubElement(certificate_node, 'ns1:{}'.format(k))
+            node.text = str(v)
+    else: #send error data
+        response_code_node.text = convert_reason_code(auth_user_dir['reason'])
 
-
-def handle_unique_login(xml_tree, rsa):
-    resp_xml = ET.Element('SOAP-ENV:Envelope')
-    body = ET.SubElement(resp_xml, 'SOAP-ENV:Body')
-    login_result = ET.SubElement(body, 'ns1:LoginUniqueNickResult')
-
-    response_code_node = ET.SubElement(login_result, 'ns1:responseCode')
-    response_code_node.text = str(LOGIN_RESPONSE_SUCCESS)
-
-
-    #auth stuff
-
-    certificate_node = ET.SubElement(login_result, 'ns1:certificate')
-
-    peerkeyprivate_node = ET.SubElement(login_result, 'ns1:peerkeyprivate')
-    peerkeyprivate_node.text = '0'
-
-    length_node = ET.SubElement(certificate_node, 'ns1:length') #???
-    length_node.text = '111'
-
-    version_node = ET.SubElement(certificate_node, 'ns1:version')
-    version_node.text = '1'
-
-    auth_user_dir = get_user_by_id(111)
-    #user info
-    for k,v in auth_user_dir.items():
-        node = ET.SubElement(certificate_node, 'ns1:{}'.format(k))
-        node.text = v
 
     #encrypted server data
     peerkeymodulus_node = ET.SubElement(certificate_node, 'ns1:peerkeymodulus')
@@ -194,8 +178,35 @@ def handle_unique_login(xml_tree, rsa):
     peerkey = {}
     peerkey['exponent'] = peerkeyexponent_node.text
     peerkey['modulus'] = peerkeymodulus_node.text
-    signature_node.text = generate_signature(rsa, int(length_node.text), int(version_node.text), auth_user_dir, peerkey, server_data, True)
+    signature_node.text = generate_signature(rsa, int(length_node.text), int(version_node.text), auth_user_dir['profile'], peerkey, server_data, True)
 
+
+    return resp_xml
+
+
+def handle_unique_login(xml_tree, rsa):
+    resp_xml = ET.Element('SOAP-ENV:Envelope')
+    body = ET.SubElement(resp_xml, 'SOAP-ENV:Body')
+    login_result = ET.SubElement(body, 'ns1:LoginUniqueNickResult')
+
+    response_code_node = ET.SubElement(login_result, 'ns1:responseCode')
+
+
+    #auth stuff
+
+    certificate_node = ET.SubElement(login_result, 'ns1:certificate')
+
+    peerkeyprivate_node = ET.SubElement(login_result, 'ns1:peerkeyprivate')
+    peerkeyprivate_node.text = '0'
+
+    length_node = ET.SubElement(certificate_node, 'ns1:length') #???
+    length_node.text = '111'
+
+    version_node = ET.SubElement(certificate_node, 'ns1:version')
+    version_node.text = '1'
+
+    
+    #get request info
     uniquenick_node = xml_tree.find('{http://gamespy.net/AuthService/}uniquenick')
     uniquenick = uniquenick_node.text
 
@@ -205,10 +216,41 @@ def handle_unique_login(xml_tree, rsa):
     namespaceid_node = xml_tree.find('{http://gamespy.net/AuthService/}partnercode')
     namespaceid = namespaceid_node.text
 
-    
+    #decrypt pw
     encrypted_pass = xml_tree.find('{http://gamespy.net/AuthService/}password').find('{http://gamespy.net/AuthService/}Value')
     password = rsa.private_decrypt(binascii.unhexlify(encrypted_pass.text), RSA.pkcs1_padding) 
-    print("Decrypted pass: {}".format(password))
+
+    #try auth
+    auth_user_dir = try_authenticate({'uniquenick': uniquenick, 'password': password, 'namespaceid': int(namespaceid), 'partnercode': int(partnercode), 'hash_type': 'plain'})
+    if 'profile' in auth_user_dir:
+        response_code_node.text = str(LOGIN_RESPONSE_SUCCESS)
+        #populate user info
+        for k,v in auth_user_dir['profile'].items():
+            node = ET.SubElement(certificate_node, 'ns1:{}'.format(k))
+            node.text = str(v)
+    else: #send error data
+        response_code_node.text = convert_reason_code(auth_user_dir['reason'])
+
+    #encrypted server data
+    peerkeymodulus_node = ET.SubElement(certificate_node, 'ns1:peerkeymodulus')
+    rsa_modulus = binascii.hexlify(rsa.n)
+    peerkeymodulus_node.text = rsa_modulus[-128:].upper()
+
+    peerkeyexponent_node = ET.SubElement(certificate_node, 'ns1:peerkeyexponent')
+    rsa_exponent = binascii.hexlify(rsa.e)
+    peerkeyexponent_node.text = rsa_exponent[-6:]
+
+    serverdata_node = ET.SubElement(certificate_node, 'ns1:serverdata')
+
+    server_data = os.urandom(128)
+    serverdata_node.text = binascii.hexlify(server_data)
+
+    signature_node = ET.SubElement(certificate_node, 'ns1:signature')
+
+    peerkey = {}
+    peerkey['exponent'] = peerkeyexponent_node.text
+    peerkey['modulus'] = peerkeymodulus_node.text
+    signature_node.text = generate_signature(rsa, int(length_node.text), int(version_node.text), auth_user_dir['profile'], peerkey, server_data, True)
 
     return resp_xml
 
@@ -233,7 +275,9 @@ def application(env, start_response):
 
     mapping_file = open("post_data.txt","a")
 
-    bio = BIO.MemoryBuffer(private_key)
+    private_key_file = open("private_key.pem","r")
+
+    bio = BIO.MemoryBuffer(private_key_file.read())
     
     rsa = RSA.load_key_bio(bio, get_pass)
     
@@ -259,5 +303,6 @@ def application(env, start_response):
     #mapping_file.write("{}\n".format(rsa))
 
     if resp != None:
+        print("Sending resp: {}".format(ET.tostring(resp, encoding='utf8', method='xml')))
         return ET.tostring(resp, encoding='utf8', method='xml')
     return [b"Hello World"]
---
+++ web/authservices/backend_auth.py
@@ -0,0 +1,121 @@
+from cgi import parse_qs, escape
+
+import jwt
+
+import MySQLdb
+
+SECRET_AUTH_KEY = "dGhpc2lzdGhla2V5dGhpc2lzdGhla2V5dGhpc2lzdGhla2V5"
+
+LOGIN_RESPONSE_SUCCESS = 0
+LOGIN_RESPONSE_SERVERINITFAILED = 1
+LOGIN_RESPONSE_USER_NOT_FOUND = 2
+LOGIN_RESPONSE_INVALID_PASSWORD = 3
+LOGIN_RESPONSE_INVALID_PROFILE = 4
+LOGIN_RESPONSE_UNIQUE_NICK_EXPIRED = 5
+LOGIN_RESPONSE_DB_ERROR = 6
+LOGIN_RESPONSE_SERVER_ERROR = 7
+
+def build_get_profile():
+    ret = "SELECT u.userid,p.profileid,u.email,p.uniquenick, p.nick from profiles p inner join users u on u.userid = p.userid"
+    return ret
+
+def get_profile_by_uniquenick(db_ctx, uniquenick):
+    query_str = build_get_profile()
+    cursor = db_ctx.cursor()
+    query_str += ' where p.uniquenick = %s and p.deleted = 0 and u.deleted = 0'
+
+    cursor.execute(query_str, (uniquenick))
+    profile = {}
+    for (userid, profileid, email, uniquenick, nick) in cursor:
+        profile['userid'] = int(userid)
+        profile['profileid'] = int(profileid)
+        profile['email'] = email
+        profile['uniquenick'] = uniquenick
+        profile['profilenick'] = nick
+        profile['namespaceid'] = 0
+        profile['partnercode'] = 0
+        profile['expiretime'] = 100000 #TODO: figure out if this value is unix timestamp, offset in seconds/ms, etc in web services sdk
+    cursor.close()
+    return profile
+
+def get_profile_by_nick_email(db_ctx, nick, email):
+    query_str = build_get_profile()
+    cursor = db_ctx.cursor()
+    query_str += ' where p.nick = %s and u.email = %s and p.deleted = 0 and u.deleted = 0'
+
+    cursor.execute(query_str, (nick, email))
+    profile = {}
+    for (userid, profileid, email, uniquenick, nick) in cursor:
+        profile['userid'] = int(userid)
+        profile['profileid'] = int(profileid)
+        profile['email'] = email
+        profile['uniquenick'] = uniquenick
+        profile['profilenick'] = nick
+        profile['namespaceid'] = 0
+        profile['partnercode'] = 0
+        profile['expiretime'] = 100000 #TODO: figure out if this value is unix timestamp, offset in seconds/ms, etc in web services sdk
+    cursor.close()
+    return profile
+def test_pass_plain_by_userid(db_ctx, userid, password):
+    auth_success = False
+    cursor = db_ctx.cursor()
+    print("Auth test: {} == {}\n".format(userid, password))
+    cursor.execute('SELECT 1 from users where userid = %s AND password = %s and deleted = 0', (userid, password))
+
+    auth_success = cursor.rowcount > 0
+
+    cursor.close()
+    return auth_success
+def application(env, start_response):
+    db_ctx = MySQLdb.connect(user='root', db='GameTracker')
+
+    # the environment variable CONTENT_LENGTH may be empty or missing
+    try:
+        request_body_size = int(env.get('CONTENT_LENGTH', 0))
+    except (ValueError):
+        request_body_size = 0
+
+    response = {}
+    response['success'] = False
+
+    # When the method is POST the variable will be sent
+    # in the HTTP request body which is passed by the WSGI server
+    # in the file like wsgi.input environment variable.
+    request_body = env['wsgi.input'].read(request_body_size)
+    jwt_decoded = jwt.decode(request_body, SECRET_AUTH_KEY, algorithm='HS256')
+
+    hash_type = 'plain'
+    if 'hash_type' in jwt_decoded:
+        hash_type = jwt_decoded['hash_type']
+
+    if 'password' not in jwt_decoded:
+        response['reason'] = LOGIN_RESPONSE_INVALID_PASSWORD
+        return jwt.encode(response, secret_auth_key, algorithm='HS256')
+
+    profile = None
+    if 'uniquenick' in jwt_decoded:
+        profile = get_profile_by_uniquenick(db_ctx, jwt_decoded['uniquenick'])
+        if profile == None:
+            response['reason'] = LOGIN_RESPONSE_INVALID_PROFILE
+    elif 'profilenick' in jwt_decoded and 'email' in jwt_decoded:
+        profile = get_profile_by_nick_email(db_ctx, jwt_decoded['profilenick'], jwt_decoded['email'])
+        if profile == None:
+            response['reason'] = LOGIN_RESPONSE_INVALID_PROFILE
+
+
+    auth_success = False
+    if hash_type == 'plain':
+        auth_success = test_pass_plain_by_userid(db_ctx, profile['userid'], jwt_decoded['password'])
+
+    if not auth_success:
+        response['reason'] = LOGIN_RESPONSE_INVALID_PASSWORD
+    else:
+        response['success'] = True
+        response['profile'] = profile
+
+
+    print("JWT Resp: {}\n".format(response))
+
+    db_ctx.close()
+
+    return jwt.encode(response, SECRET_AUTH_KEY, algorithm='HS256')
