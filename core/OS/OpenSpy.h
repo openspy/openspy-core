e6a810425809d21cb66198440a4046874d14d096
Andrew Learn <chcniz@gmail.com>
2017-08-26 13:07:18 -0400

    removed use epoll def from win32/select from linux

--- core/OS/OpenSpy.h
+++ core/OS/OpenSpy.h
@@ -9,7 +9,7 @@
 #include "Threads/Win32/WinThread.h"
 #include "Threads/Win32/Win32Mutex.h"
 #define EVTMGR_USE_SELECT 1
-#define EVTMGR_USE_EPOLL 0
+//#define EVTMGR_USE_EPOLL 0
 typedef int socklen_t;
 #ifndef snprintf
 	#define snprintf sprintf_s
@@ -20,7 +20,7 @@ typedef int socklen_t;
 #define close closesocket
 int gettimeofday(struct timeval *tp, struct timezone *tzp);
 #else
-#define EVTMGR_USE_SELECT 0
+//#define EVTMGR_USE_SELECT 0
 #define EVTMGR_USE_EPOLL 1
 #include <unistd.h>
 #include <sys/socket.h>
