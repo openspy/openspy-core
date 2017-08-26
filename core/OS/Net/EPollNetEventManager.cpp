8b010c01047a7466c746b6de6f4c8d5baea6838d
Andrew Learn <chcniz@gmail.com>
2017-08-26 12:49:08 -0400

    fixed epoll bugs

--- core/OS/Net/EPollNetEventManager.cpp
+++ core/OS/Net/EPollNetEventManager.cpp
@@ -8,24 +8,28 @@
 
 	EPollNetEventManager::EPollNetEventManager() {
 		m_exit_flag = false;
-		
+
 		m_epollfd = epoll_create(MAX_EPOLL_EVENTS);
-		
+
 		m_added_drivers = false;
 	}
 	EPollNetEventManager::~EPollNetEventManager() {
 		m_exit_flag = true;
-		
+		std::map<void *, EPollDataInfo *>::iterator it =  m_datainfo_map.begin();
+		while(it != m_datainfo_map.end()) {
+			EPollDataInfo *data = (*it).second;
+			free((void *)data);
+			it++;
+		}
 		close(m_epollfd);
 	}
 	void EPollNetEventManager::run() {
 		if(!m_added_drivers) {
 			setupDrivers();
 		}
-		int nr_events = epoll_wait (m_epollfd, &m_events, MAX_EPOLL_EVENTS, EPOLL_TIMEOUT);
-		
+		int nr_events = epoll_wait (m_epollfd, (epoll_event *)&m_events, MAX_EPOLL_EVENTS, EPOLL_TIMEOUT);
 		for(int i=0;i<nr_events;i++) {
-			EPollDataInfo *data = (EPollDataInfo *)m_events[i].ptr;
+			EPollDataInfo *data = (EPollDataInfo *)m_events[i].data.ptr;
 			if(data->is_peer) {
 				INetPeer *peer = (INetPeer *)data->ptr;
 				peer->think(true);
@@ -34,7 +38,7 @@
 				driver->think(true);
 			}
 		}
-		
+
 		//force peer think false for ping logic, etc
 		std::vector<INetDriver *>::iterator it = m_net_drivers.begin();
 		while(it != m_net_drivers.end()) {
@@ -45,27 +49,27 @@
 	}
 	void EPollNetEventManager::RegisterSocket(INetPeer *peer) {
 		EPollDataInfo *data_info = (EPollDataInfo *)malloc(sizeof(EPollDataInfo));
-		
+
 		struct epoll_event ev;
 		ev.events = 0;
 		ev.data.ptr = data_info;
 
 		data_info->ptr = peer;
 		data_info->is_peer = true;
-		
+
 		m_datainfo_map[peer] = data_info;
-		
-		epoll_ctl(m_epollfd, EPOLL_CTL_ADD, peer->getSocket(), &ev);
+
+		epoll_ctl(m_epollfd, EPOLL_CTL_ADD, peer->GetSocket(), &ev);
 	}
 	void EPollNetEventManager::UnregisterSocket(INetPeer *peer) {
 		if(m_datainfo_map.find(peer) != m_datainfo_map.end()) {
 			struct epoll_event ev;
-			ev.event = 0;
+			ev.events = EPOLLIN | EPOLLET;
 			ev.data.ptr = peer;
-			epoll_ctl(m_epollfd, EPOLL_CTL_DEL, peer->getSocket(), &ev);
+			epoll_ctl(m_epollfd, EPOLL_CTL_DEL, peer->GetSocket(), &ev);
 			free((void *)m_datainfo_map[peer]);
 		}
-		
+
 	}
 
 	void EPollNetEventManager::setupDrivers() {
@@ -73,18 +77,18 @@
 		std::vector<INetDriver *>::iterator it = m_net_drivers.begin();
 		while(it != m_net_drivers.end()) {
 			INetDriver *driver = *it;
-			
+
 			EPollDataInfo *data_info = (EPollDataInfo *)malloc(sizeof(EPollDataInfo));
-			
+
 			data_info->ptr = driver;
 			data_info->is_peer = false;
-			
+
 			struct epoll_event ev;
-			ev.events = 0;
-			ev.data.ptr = data_info;	
-			
+			ev.events = EPOLLIN | EPOLLET;
+			ev.data.ptr = data_info;
+
 			m_datainfo_map[driver] = data_info;
-			
+
 			epoll_ctl(m_epollfd, EPOLL_CTL_ADD, driver->getListenerSocket(), &ev);
 			it++;
 		}
