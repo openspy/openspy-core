3bb96bb4639089e998549fe76e6627c32b2bf296
Andrew Learn <chcniz@gmail.com>
2017-11-26 00:06:37 -0500

    optimization of select net event mgr

--- core/OS/Net/SelectNetEventManager.cpp
+++ core/OS/Net/SelectNetEventManager.cpp
@@ -8,77 +8,73 @@
 SelectNetEventManager::SelectNetEventManager() {
 	m_exit_flag = false;
 	m_dirty_fdset = true;
+	mp_mutex = OS::CreateMutex();
 }
 SelectNetEventManager::~SelectNetEventManager() {
 	m_exit_flag = true;
+	delete mp_mutex;
 }
 void SelectNetEventManager::run() {
-	
+
 	struct timeval timeout;
 
-    memset(&timeout,0,sizeof(struct timeval));
-    timeout.tv_sec = SELECT_TIMEOUT;
+	memset(&timeout, 0, sizeof(struct timeval));
+	timeout.tv_sec = SELECT_TIMEOUT;
 
 	int hsock = setup_fdset();
 
-    if(select(hsock, &m_fdset, NULL, NULL, &timeout) < 0) {
-    	//return;
-    }
-    if(m_exit_flag) {
-    	return;
-    }
+	if (select(hsock, &m_fdset, NULL, NULL, &timeout) < 0) {
+		//return;
+	}
+	if (m_exit_flag) {
+		return;
+	}
+	mp_mutex->lock();
 
 	std::vector<INetDriver *>::iterator it = m_net_drivers.begin();
-	while(it != m_net_drivers.end()) {
+	while (it != m_net_drivers.end()) {
 		INetDriver *driver = *it;
-		if(FD_ISSET(driver->getListenerSocket(), &m_fdset))
+		if (FD_ISSET(driver->getListenerSocket(), &m_fdset))
 			driver->think(true);
-
-		std::vector<INetPeer *> net_peers = driver->getPeers();
-		std::vector<INetPeer *>::iterator it2 = net_peers.begin();
-		while (it2 != net_peers.end()) {
-			INetPeer *peer = *it2;
-			int sd = peer->GetSocket();
-			if(sd != driver->getListenerSocket() && FD_ISSET(sd, &m_fdset))
-				peer->think(true);
-			it2++;
-		}
 		it++;
 	}
+
+	std::vector<INetPeer *>::iterator it2 = m_peers.begin();
+	while (it2 != m_peers.end()) {
+		INetPeer *peer = *it2;
+		int sd = peer->GetSocket();
+		if (sd != peer->GetDriver()->getListenerSocket() && FD_ISSET(sd, &m_fdset))
+			peer->think(true);
+		it2++;
+	}
+	mp_mutex->unlock();
 }
 int SelectNetEventManager::setup_fdset() {
-	
-	FD_ZERO(&m_fdset);
 	int hsock = -1;
+	mp_mutex->lock();
+	FD_ZERO(&m_fdset);
 	if (m_dirty_fdset) {
 		m_cached_sockets.clear();
 		std::vector<INetDriver *>::iterator it = m_net_drivers.begin();
 		while (it != m_net_drivers.end()) {
 			INetDriver *driver = *it;
-
-			std::vector<int> sockets = driver->getSockets();
-			std::vector<int>::iterator it2 = sockets.begin();
 			int sd = driver->getListenerSocket();
-			FD_SET(sd, &m_fdset);
 			m_cached_sockets.push_back(sd);
-			if (sd > hsock) {
-				hsock = sd;
-			}
-			while (it2 != sockets.end()) {
-				sd = *it2;
-				if (sd > hsock) {
-					hsock = sd;
-				}
-				if (sd != driver->getListenerSocket()) {
-					FD_SET(sd, &m_fdset);
-					m_cached_sockets.push_back(sd);
-				}
-				it2++;
-			}
+			FD_SET(sd, &m_fdset);
 			it++;
+		}
+
+		std::vector<INetPeer*>::iterator it2 = m_peers.begin();
+		while (it2 != m_peers.end()) {
+			INetPeer *peer = *it2;
+			int sd = peer->GetSocket();
+			m_cached_sockets.push_back(sd);
+			FD_SET(sd, &m_fdset);
+			it2++;
 		}
 		m_hsock = hsock + 1;
 		m_dirty_fdset = false;
+		mp_mutex->unlock();
 		return m_hsock;
 	}
 	else {
@@ -92,9 +88,19 @@ int SelectNetEventManager::setup_fdset() {
 	}
 }
 void SelectNetEventManager::RegisterSocket(INetPeer *peer) {
+	mp_mutex->lock();
 	m_dirty_fdset = true;
+	m_peers.push_back(peer);
+	mp_mutex->unlock();
 }
 void SelectNetEventManager::UnregisterSocket(INetPeer *peer) {
+	mp_mutex->lock();
+	std::vector<INetPeer *>::iterator it = std::find(m_peers.begin(), m_peers.end(), peer);
+	if (it != m_peers.end()) {
+		m_peers.erase(it);
+	}
+
 	m_dirty_fdset = true;
+	mp_mutex->unlock();
 }
 #endif
