e364c184033b4f7eb57260735bbbb61edf8e6db1
Andrew Learn <chcniz@gmail.com>
2018-01-18 20:37:54 -0500

    removed gettimeofday from SM main

--- search/main.cpp
+++ search/main.cpp
@@ -29,11 +29,6 @@ int main() {
 		WSAStartup(MAKEWORD(1, 0), &wsdata);
 	#endif
 
-    /*OS::Init("search", 8, "chc");
-	
-	g_gameserver = new SM::Server();
-    g_driver = new SM::Driver(g_gameserver, "0.0.0.0", SM_SERVER_PORT);
-	g_gameserver->addNetworkDriver(g_driver);*/
 	OS::Init("SM", "openspy.cfg");
 
 	g_gameserver = new SM::Server();
@@ -69,29 +64,3 @@ void shutdown() {
         g_running = false;
     }
 }
-
-
-#ifdef _WIN32
-
-int gettimeofday(struct timeval * tp, struct timezone * tzp)
-{
-    // Note: some broken versions only have 8 trailing zero's, the correct epoch has 9 trailing zero's
-    static const uint64_t EPOCH = ((uint64_t) 116444736000000000ULL);
-
-    SYSTEMTIME  system_time;
-    FILETIME    file_time;
-    uint64_t    time;
-
-    GetSystemTime( &system_time );
-    SystemTimeToFileTime( &system_time, &file_time );
-    time =  ((uint64_t)file_time.dwLowDateTime )      ;
-    time += ((uint64_t)file_time.dwHighDateTime) << 32;
-
-    tp->tv_sec  = (long) ((time - EPOCH) / 10000000L);
-    tp->tv_usec = (long) (system_time.wMilliseconds * 1000);
-    return 0;
-}
-
-
-#endif
-
