538b9b575425e4be2682c5f77c515b5521030413
Andrew Learn <chcniz@gmail.com>
2018-02-17 23:54:43 -0500

    fixed insecure encryptedInfo data being returned

--- FESL/server/FESLDriver.cpp
+++ FESL/server/FESLDriver.cpp
@@ -326,7 +326,7 @@ namespace FESL {
 		unsigned char *buf = (unsigned char *)malloc(mem_len);
 
 		OS::Base64StrToBin(input.c_str(), &b64_out, out_len);
-		RSA_public_decrypt(out_len, b64_out,buf, m_encrypted_login_info_key, RSA_PKCS1_PADDING);
+		RSA_private_decrypt(out_len, b64_out,buf, m_encrypted_login_info_key, RSA_PKCS1_PADDING);
 		ret = std::string((char *)buf);
 		if (b64_out)
 			free((void *)b64_out);
@@ -340,7 +340,7 @@ namespace FESL {
 		std::string ret;
 		int mem_len = RSA_size(m_encrypted_login_info_key);
 		unsigned char *buf = (unsigned char *)malloc(mem_len);
-		RSA_private_encrypt(input.length()+1, (const unsigned char *)input.c_str(),buf, m_encrypted_login_info_key, RSA_PKCS1_PADDING);
+		RSA_public_encrypt(input.length()+1, (const unsigned char *)input.c_str(),buf, m_encrypted_login_info_key, RSA_PKCS1_PADDING);
 		const char *b64_str = OS::BinToBase64Str(buf, mem_len);
 		ret = std::string(b64_str);
 		free((void *)b64_str);
