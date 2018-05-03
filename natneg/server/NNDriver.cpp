55c57b06cd57eb1b4940a4e040fe911149c009ec
Andrew Learn <chcniz@gmail.com>
2018-05-03 18:44:53 -0400

    nn udp fix

--- natneg/server/NNDriver.cpp
+++ natneg/server/NNDriver.cpp
@@ -74,7 +74,10 @@ namespace NN {
 			while (it != datagrams.end()) {
 				INetIODatagram dgram = *it;
 				Peer *peer = NULL;
-				if(dgram.buffer.size() == 0) continue;
+				if (dgram.comm_len == 0) {
+					it++;
+					continue;
+				}
 				if (dgram.error_flag) {
 					peer = find_client(dgram.address);
 					if (peer) {
