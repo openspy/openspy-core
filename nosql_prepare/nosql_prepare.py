3458ca86be837653b3e366b1015d18fc338c0cfd
Andrew Learn <chcniz@gmail.com>
2018-06-01 14:44:43 -0400

    updated nosql_prepare and web backend for removed group fields

--- nosql_prepare/nosql_prepare.py
+++ nosql_prepare/nosql_prepare.py
@@ -28,19 +28,17 @@ for (id, gamename, secretkey, description) in cursor:
 cursor.close()
 
 cursor = db.cursor()
-cursor.execute("SELECT grp.gameid `gameid`, grp.groupid `groupid`,grp.name `name`,grp.numservers `numservers`,grp.numwaiting `numwaiting`,grp.password `pass`,g.gamename `gamename`, grp.maxwaiting `maxwaiting`, grp.other `other` from grouplist grp INNER JOIN games g on g.id = grp.gameid")
+cursor.execute("SELECT grp.gameid `gameid`, grp.groupid `groupid`,grp.name `name`,g.gamename `gamename`, grp.maxwaiting `maxwaiting`, grp.other `other` from grouplist grp INNER JOIN games g on g.id = grp.gameid")
 
 
 redis_ctx = redis.StrictRedis(host='localhost', port=6379, db = 1)
 
-for (gameid, groupid, name, numservers, numwaiting, password, gamename, maxwaiting, other) in cursor:
+for (gameid, groupid, name, gamename, maxwaiting, other) in cursor:
 	redis_key = "{}:{}:".format(gamename, groupid)
 	redis_ctx.hset(redis_key,"gameid", gameid)
 	redis_ctx.hset(redis_key,"groupid", groupid)
 	redis_ctx.hset(redis_key,"maxwaiting", maxwaiting)
 	redis_ctx.hset(redis_key,"hostname", name)
-	redis_ctx.hset(redis_key,"password", password)
-	redis_ctx.hset(redis_key,"numwaiting", numwaiting)
 
 	other_params = other.split('\\');
 	key = None
