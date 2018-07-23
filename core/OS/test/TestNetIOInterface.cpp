0c48a95727d2764427cbf4341e4048cfc9a46934
Andrew Learn <chcniz@gmail.com>
2018-07-22 22:07:55 -0400

    added cmake tests + linux fix

--- core/OS/test/TestNetIOInterface.cpp
+++ core/OS/test/TestNetIOInterface.cpp
@@ -1,15 +1,19 @@
 #include "TestNetIOInterface.h"
 
 namespace Test {
+	template<>
 	void TestNetIOInterface<>::fake_AddTCPConnection(INetIOSocket *socket) {
 		m_TCPAccept_WaitList.push_back(socket);
 	}
+	template<>
     void TestNetIOInterface<>::fake_PushIncomingPacket(INetIOSocket *socket, OS::Buffer &buffer) {
         m_pending_packets[socket].push_back(buffer);
     }
+	template<>
 	void TestNetIOInterface<>::fake_SetSendCallback(bool (*SendCallback)(INetIOSocket *, OS::Buffer)) {
 		mp_SendCallback = SendCallback;
 	}
+	template<>
 	void TestNetIOInterface<>::fake_TCPDisconnect(INetIOSocket *socket) {
 		m_pending_tcp_disconnects.push_back(socket);
 	}
