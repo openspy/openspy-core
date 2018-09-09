e961c2c20632b448913038baa0009baf25e2ff18
Andrew Learn <chcniz@gmail.com>
2018-09-09 11:41:19 -0400

    added missing redis publish call in buddy add

--- web/authservices/AccountService/UserProfileMgrService.py
+++ web/authservices/AccountService/UserProfileMgrService.py
@@ -222,13 +222,12 @@ class UserProfileMgrService(BaseService):
                 if self.redis_presence_ctx.hdel(redis_key, request_data["to_profileid"]):
                     success = True
 
-                    print("Process request: {}\n".format(request_data))
-
                     to_profile_model = Profile.get((Profile.id == request_data["to_profileid"]))
                     from_profile_model = Profile.get((Profile.id == request_data["from_profileid"]))
 
                     Buddy.insert(from_profile=to_profile_model,to_profile=from_profile_model).execute()
                     publish_data = "\\type\\authorize_add\\from_profileid\\{}\\to_profileid\\{}".format(request_data["from_profileid"], request_data["to_profileid"])
+                    self.redis_presence_ctx.publish(self.redis_presence_channel, publish_data)
 
                     Buddy.insert(from_profile=from_profile_model,to_profile=to_profile_model).execute()
                     publish_data = "\\type\\authorize_add\\from_profileid\\{}\\to_profileid\\{}".format(request_data["to_profileid"], request_data["from_profileid"])
