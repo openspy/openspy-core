3e9c45c5d05e3357ac627923819f624b4b325909
Andrew Learn <chc@thmods.com>
2019-02-23 00:33:14 -0500

    added keymaster db

--- sql/KeyMaster.sql
+++ sql/KeyMaster.sql
@@ -1,6 +1,16 @@
+use keymaster;
+create table cdkeys ( 
+	id int auto_increment PRIMARY KEY,
+	cdkey text NOT NULL,
+    user_inserted tinyint(1) not null,
+	gameid int not null
+);
 create table profile_cdkeys (
 	id int auto_increment PRIMARY KEY,
-	cdkey varchar(65),
-	profileid int,
-    gameid int
+	cdkey_id int not null,
+	profileid int not null
+);
+create table cdkey_rules (
+	gameid int not null,
+	fail_if_not_found tinyint(1) not null
 );
