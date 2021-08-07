0c250038185eea105becbd32fa477760689fa1dc
Andrew Learn <chcniz@gmail.com>
2021-08-07 12:21:11 -0400

    Revert "made delete not user peer"
    This reverts commit ae16ca35f6d39b565ee90033287676a82d61bca2.

--- code/peerchat/tasks/Perform_DeleteUser.cpp
+++ code/peerchat/tasks/Perform_DeleteUser.cpp
@@ -7,7 +7,7 @@ namespace Peerchat {
 	bool Perform_DeleteUser(PeerchatBackendRequest request, TaskThreadData* thread_data) {
         TaskResponse response;
 
-        UserSummary userDetails = request.summary;
+        UserSummary userDetails = request.peer->GetUserDetails();
 
         std::string formatted_name;
 		std::transform(userDetails.nick.begin(),userDetails.nick.end(),std::back_inserter(formatted_name),tolower);
@@ -15,13 +15,21 @@ namespace Peerchat {
         Redis::Command(thread_data->mp_redis_connection, 0, "DEL usernick_%s", formatted_name.c_str());
         Redis::Command(thread_data->mp_redis_connection, 0, "DEL user_%d", userDetails.id);
 
-        std::vector<int>::iterator it = request.channel_id_list.begin();
-        while (it != request.channel_id_list.end()) {
+        std::vector<int> channels = request.peer->GetChannels();
+        std::vector<int>::iterator it = channels.begin();
+        while (it != channels.end()) {
             int channel_id = *it;
             ChannelSummary channelSummary = LookupChannelById(thread_data, channel_id);
             RemoveUserFromChannel(thread_data, userDetails, channelSummary, "NA", "NA", userDetails, true);
             it++;
         }
+
+        response.error_details.response_code = TaskShared::WebErrorCode_Success;
+
+        if(request.callback)
+            request.callback(response, request.peer);
+		if (request.peer)
+			request.peer->DecRef();
 		return true;
 	}
 }
