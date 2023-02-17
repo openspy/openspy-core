e4d7e26e827ea862fd49e382bd3aa86bb21d92c9
CHC NiZ <chcniz@gmail.com>
2023-02-17 16:53:57 -0500

    switched to hmget

--- code/utmaster/tasks/PerformListServers.cpp
+++ code/utmaster/tasks/PerformListServers.cpp
@@ -45,52 +45,138 @@ namespace MM {
 
 		return 0;
 	}
-    ServerRecord LoadServerInfo(UTMasterRequest request, TaskThreadData *thread_data, std::string server_key) {
+	void LoadBasicServerInfo(UTMasterRequest request, TaskThreadData* thread_data, std::string server_key, ServerRecord &result) {
+
+		std::vector<std::string> lookup_keys;
+
+		lookup_keys.push_back("id");
+		lookup_keys.push_back("gameid");
+		lookup_keys.push_back("wan_port");
+		lookup_keys.push_back("wan_ip");
+		lookup_keys.push_back("mutators");
+
+		std::string command = "HMGET " + server_key;
+
+		std::vector<std::string>::iterator it2 = lookup_keys.begin();
+		
+		while (it2 != lookup_keys.end()) {
+			command += " " + *it2;
+			it2++;
+		}
+
+		Redis::Response reply = Redis::Command(thread_data->mp_redis_connection, 0, command.c_str());
+		if (reply.values.size() == 0 || reply.values.front().type == Redis::REDIS_RESPONSE_TYPE_ERROR || reply.values[0].arr_value.values.size() < 2)
+			return;
+
+		std::vector<std::pair<Redis::REDIS_RESPONSE_TYPE, Redis::Value> > values = reply.values[0].arr_value.values;
+		std::vector<std::pair<Redis::REDIS_RESPONSE_TYPE, Redis::Value> >::iterator v_it = values.begin();
+		int idx = 0;
+		while (v_it != values.end()) {
+			std::pair<Redis::REDIS_RESPONSE_TYPE, Redis::Value> p = *v_it;
+			v_it++;
+
+			if (p.first != Redis::REDIS_RESPONSE_TYPE_STRING) {
+				continue;
+			}
+			switch (idx++) {
+			case 0:
+				result.id = atoi(p.second.value._str.c_str());
+				break;
+			case 1:
+				result.gameid = atoi(p.second.value._str.c_str());
+				break;
+			case 2:
+				result.m_address.port = atoi(p.second.value._str.c_str());
+				break;
+			case 3:
+				result.m_address.ip = inet_addr(p.second.value._str.c_str());
+				break;
+			case 4:
+				result.m_mutators = OS::KeyStringToVector(p.second.value._str);
+				break;
+			}
+		}
+	}
+	ServerRecord LoadServerInfo(UTMasterRequest request, TaskThreadData* thread_data, std::string server_key) {
 		Redis::Response reply;
 		Redis::Value v, arr;
 
-        ServerRecord result;
-		
-		result.id = GetHKeyInt(thread_data, server_key, "id");
-		result.gameid = GetHKeyInt(thread_data, server_key, "gameid");
+		ServerRecord result;
 
-		//check if its from gamespy query
-		int port = GetPortFromGamespyInfo(thread_data, server_key);
-		if(port == 0)
-			port = GetHKeyInt(thread_data, server_key, "wan_port");
-
-		result.m_address.port = port;
-		result.m_address.ip = inet_addr(GetHKey(thread_data, server_key, "wan_ip").c_str());
-
-		//check if its a gamespy query connection, update wan port if so
-
-		std::string mutators = GetHKey(thread_data, server_key, "mutators");
-		if(mutators.length() > 0) {
-			result.m_mutators = OS::KeyStringToVector(mutators);
-		}
-
-		std::string cust_keys = server_key + "custkeys";
-
-		result.hostname = GetHKey(thread_data, cust_keys, "hostname");
-		result.level = GetHKey(thread_data, cust_keys, "mapname");
-		result.game_group = GetHKey(thread_data, cust_keys, "gametype");
-		result.bot_level = GetHKey(thread_data, cust_keys, "botlevel");
-		result.num_players = GetHKeyInt(thread_data, cust_keys, "numplayers");
-		result.max_players = GetHKeyInt(thread_data, cust_keys, "maxplayers");
-
-		const char* required_keys[] = {"GamePassword", "GameStats", "ServerVersion", "ServerMode"};
-		for (int i = 0; i < sizeof(required_keys) / sizeof(const char*); i++) {
-			const char* p = required_keys[i];
-			result.m_rules[p] = GetHKey(thread_data, cust_keys, p);
-		}
+		std::vector<std::string> lookup_keys;
 
 
-		//load filtered stuff
+		LoadBasicServerInfo(request, thread_data, server_key, result);
+
+
+
+		//These are returned in order for parsing, first 6 are specially handled
+		lookup_keys.push_back("hostname"); //0
+		lookup_keys.push_back("mapname"); //1
+		lookup_keys.push_back("gametype"); //2
+		lookup_keys.push_back("botlevel");//3
+		lookup_keys.push_back("numplayers"); //4
+		lookup_keys.push_back("maxplayers");//5
+		lookup_keys.push_back("GamePassword");
+		lookup_keys.push_back("GameStats");
+		lookup_keys.push_back("ServerVersion");
+		lookup_keys.push_back("ServerMode");
+
 		std::vector<FilterProperties>::iterator it = request.m_filters.begin();
 		while (it != request.m_filters.end()) {
 			FilterProperties p = *it;
-			result.m_rules[p.field] = GetHKey(thread_data, cust_keys, p.field);
+			lookup_keys.push_back(p.field);
 			it++;
+		}
+
+		std::string cust_keys = server_key + "custkeys";
+		std::string command = "HMGET " + cust_keys;
+
+		std::vector<std::string>::iterator it2 = lookup_keys.begin();
+		while (it2 != lookup_keys.end()) {
+			command += " " + *it2;
+			it2++;
+		}
+
+		reply = Redis::Command(thread_data->mp_redis_connection, 0, command.c_str());
+		if (reply.values.size() == 0 || reply.values.front().type == Redis::REDIS_RESPONSE_TYPE_ERROR || reply.values[0].arr_value.values.size() < 2)
+			return result;
+
+		std::vector<std::pair<Redis::REDIS_RESPONSE_TYPE, Redis::Value> > values = reply.values[0].arr_value.values;
+		std::vector<std::pair<Redis::REDIS_RESPONSE_TYPE, Redis::Value> >::iterator v_it = values.begin();
+		int idx = 0;
+		while (v_it != values.end()) {
+			std::pair<Redis::REDIS_RESPONSE_TYPE, Redis::Value> p = *v_it;
+			v_it++;
+
+			if (p.first != Redis::REDIS_RESPONSE_TYPE_STRING) {
+				continue;
+			}
+			std::string field_name = lookup_keys.at(idx);
+			switch (idx++) {
+				case 0:
+					result.hostname = p.second.value._str;
+					break;
+				case 1:
+					result.level = p.second.value._str;
+					break;
+				case 2:
+					result.game_group = p.second.value._str;
+					break;
+				case 3:
+					result.bot_level = p.second.value._str;
+					break;
+				case 4:
+					result.num_players = atoi(p.second.value._str.c_str());
+					break;
+				case 5:
+					result.max_players = atoi(p.second.value._str.c_str());
+					break;
+				default: //not specially managed property, dump into rules
+					result.m_rules[field_name] = p.second.value._str;
+					break;
+			}
+
 		}
         return result;
     }
