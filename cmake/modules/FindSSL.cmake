65695f9e112c0af6cf57a140bf39bf6d40e69f59
CHC NiZ <chcniz@gmail.com>
2023-11-10 00:38:37 -0500

    added openssl to cmake

--- cmake/modules/FindSSL.cmake
+++ cmake/modules/FindSSL.cmake
@@ -3,6 +3,7 @@ find_package(OpenSSL)
 IF(OPENSSL_FOUND)
     SET(SSL_LIBS "${OPENSSL_LIBRARIES}")
     SET(SSL_OS_INC "${CMAKE_CURRENT_SOURCE_DIR}/../core/SSL/OpenSSL")
+    SET(SSL_INCS "${OPENSSL_INCLUDE_DIR}")
     file (GLOB SSL_SRCS "SSL/OpenSSL/SSL/*.cpp")
     
 ELSE()
