b83de88bbfb595eca576850bb0df1a35ee90325e
CHC NiZ <chcniz@gmail.com>
2023-12-26 10:30:34 -0500

    misc cleanup

--- code/core/OS/OpenSpy.cpp
+++ code/core/OS/OpenSpy.cpp
@@ -185,11 +185,8 @@ namespace OS {
 	std::string str_tolower(std::string s)
 	{
 		std::transform(s.begin(), s.end(), s.begin(), 
-					// static_cast<int(*)(int)>(std::tolower)         // wrong
-					// [](int c){ return std::tolower(c); }           // wrong
-					// [](char c){ return std::tolower(c); }          // wrong
-					[](unsigned char c){ return std::tolower(c); } // correct
-					);
+			[](unsigned char c){ return std::tolower(c); }
+		);
 		return s;
 	}
 
